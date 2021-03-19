961eb3a907296c1495fdc73294b9a08bf262dce7
harshH-addweb <harsh.h.addweb@gmail.com>
2021-03-19 18:15:55 +0530

    readme and setup updated

--- README.md
+++ README.md
@@ -9,28 +9,27 @@
 ```
 <br>
 
-#### First time running the script
-- If you are running lando for first time the make sure `Are you doing setup for the first time [yes]/no:`  is yes
-- It will ask for db name with path. Enter the db name / with path
-  
-Eg: `Enter the DB name with path: ./db.sql / ./db.zip` (or)
-&nbsp; &nbsp; &nbsp;  `Enter the DB name with path: /home/user_name/<path>/db_file`
+- Also copy the database file where your `.lando.yml` file exists 
 
+#### First time running the setup
+`Are you doing setup for the first time [yes]/no: yes`
+
+- It will ask for database file name. `Enter the Database file name: DATABASE_FILE_NAME`
 - It will also check whether lando is installed in your system or not. If not script will install lando for you.
 <br>
 
-#### Not first time running the script
-
+#### Not first time running the setup
+`Are you doing setup for the first time [yes]/no: no`
 - If you are not running the script for first time then it will ask for database export and import
 ```
-Do you want to Backup Existing DB [yes]/no: yes
-Do you want to Import Database yes/[no]: yes
+Do you want to Import Database [yes]/no: yes
+Do you want to Export Database yes/[no]: yes
 ```
-- If you have entered both yes then it will export and import the database. And also ask for DB name, mention as above example.
+- If you have entered both yes then it will export and import the database. And also ask for database name as mentioned as above.
 - If you have entered one of then yes then it will import / export the database
 <br>
 
-##### At last it will show you the lando info with table format
+##### At last it will show you the lando info in table format
 
 
 ![Lando_Info](/images/lando_info.png)
--- setup.sh
+++ setup.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
 lando_url="https://raw.githubusercontent.com/harshH-addweb/shell-script/main/ubuntu/lando-setup.sh"
 
+#Color Variables
+reset='\033[0m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+#Bold Colors
+Breset='\033[0m'
+Bred='\033[1;31m'
+Bgreen='\033[1;32m'
+Byellow='\033[1;33m'
+Bblue='\033[1;34m'
+Bmagenta='\033[1;35m'
+Bcyan='\033[1;36m'
+
+#Usage Fucntion
+usage()
+{
+    echo -e "\n ${red}Please enter a valid entry ${Bred}[yes/no]${reset}"
+    exit 1
+}
+
 read -e -p "Are you doing setup for the first time [yes]/no: " -i "yes" first_setup
 
 
-if [ $first_setup == 'yes' ]
+if [ $first_setup == 'yes' ];
 then
     import_db=yes
-    read -e -p "Enter the DB name with path: " db_name
+    read -e -p "Enter the Database file name: " db_name
+    
     
     # Check whether lando is installed or not
-    if [ -f /usr/local/bin/lando ]
+    if [ -f /usr/local/bin/lando ];
     then
         # Lando Start & DB import
-        echo -e "\n\033[0;32m ........Lando Start & DB import........\033[0m \n"
+        echo -e "\n${green} ........Lando Start & DB import........${reset} \n"
         lando start
         lando db-import $db_name
     else
@@ -22,45 +48,52 @@ then
         /bin/bash -c "$(curl -sL $lando_url)"
 
         # Lando Start & DB import
-        echo -e "\n\033[0;32m ........Lando Start & DB import........\033[0m \n"
+        echo -e "\n${green} ........Lando Start & DB import........${reset} \n"
         lando start
         lando db-import $db_name
     fi
-else
-    read -e -p "Do you want to Backup Existing DB [yes]/no: " -i "yes" db_backup
-    read -e -p "Do you want to Import Database yes/[no]: " -i "no" import_db
 
-    if [ $import_db == 'yes' ] && [ $db_backup == 'yes' ];
+elif [ $first_setup == 'no' ];
+then
+    read -e -p "Do you want to Import Database [yes]/no: " -i "yes" import_db
+    read -e -p "Do you want to Export Database yes/[no]: " -i "no" export_db
+
+    if [ $import_db == 'yes' ] && [ $export_db == 'yes' ];
     then
-        read -e -p "Enter the DB name with path: " db_name
+        read -e -p "Enter the Database file name: " db_name
         
         # Exporting Database
-        echo -e "\n\033[0;36m ........Exporting Database........\033[0m \n"
+        echo -e "\n${cyan} ........Exporting Database........${reset} \n"
         lando db-export
 
         # Lando Restarting & Importing Database
-        echo -e "\n\033[0;33m ........Lando Restarting & Importing Database........\033[0m \n"
+        echo -e "\n${yellow} ........Lando Restarting & Importing Database........${reset} \n"
         lando rebuild -y
         lando db-import $db_name
 
-    elif [ $import_db == 'yes' ] && [ $db_backup == 'no' ];
+    elif [ $import_db == 'yes' ] && [ $export_db == 'no' ];
     then
-        read -e -p "Enter the DB name with path: " db_name
+        read -e -p "Enter the Database file name: " db_name
 
         # Lando Restarting & Importing Database
-        echo -e "\n\033[0;33m ........Lando Restarting & Importing Database........\033[0m \n"
+        echo -e "\n${yellow} ........Lando Restarting & Importing Database........${reset} \n"
         lando rebuild -y
         lando db-import $db_name
 
-    elif [ $import_db == 'no' ] && [ $db_backup == 'yes' ];
+    elif [ $import_db == 'no' ] && [ $export_db == 'yes' ];
     then
         # Exporting Database
-        echo -e "\n\033[0;36m ........Exporting Database........\033[0m \n"
+        echo -e "\n${cyan} ........Exporting Database........${reset} \n"
         lando db-export
+    elif [ $import_db == 'no' ] && [ $export_db == 'no' ];
+    then
+        echo -e "\nYou haven't choosen to import db but you can import it later but running ${Bmagenta}lando db-import <file-path>${reset} in your terminal.\n"
+        echo -e "\nYou also haven't choosen for db backup but you can backup db by running ${Bmagenta}lando db-export${reset} in your terminal.\n"
     else
-        echo -e "\nYou haven't choosen to import db but you can import it later but running \033[1;35mlando db-import <file-path>\033[0m in your terminal.\n"
-        echo -e "\nYou also haven't choosen for db backup but you can backup db by running \033[1;35mlando db-export\033[0m in your terminal.\n"
+        usage
     fi  
+else
+    usage
 fi
 
 
